4732e270953834c52520d8db47dba47fcef5d8c4
Eduardo Romero <38575968+wichoedu16@users.noreply.github.com>
2025-01-07 15:59:05 -0500

    agregar input a botones

--- src/app/main-button/main-button.component.ts
+++ src/app/main-button/main-button.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-button',
   templateUrl: './main-button.component.html',
   styleUrls: ['./main-button.component.css']
 })
-export class MainButtonComponent {
 
+export class MainButtonComponent {
+  
+  @Input() titulo: string = 'Buton n'
 }
